31df50b5bd7498a4afafc0c92a69f7cf8588de19
nabil <none@none.com>
2022-09-29 12:07:42 +0200

    message de commit

--- src/view/home.php
+++ src/view/home.php
@@ -2,6 +2,6 @@
 
 <p>Texte du mec qui doit ecrire les règles</p>
 
-<form action="question.php" method="post">
+<form action="../view/question.php" method="post">
 	<button type="submit">JOUER</button>
 </form>
