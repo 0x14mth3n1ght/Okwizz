c0043bfd06d0df4f481c051c423faf4cc6da9dc0
night <none@none.com>
2022-09-29 12:40:15 +0200

    Us: utilisateur voit une question

--- src/view/question.php
+++ src/view/question.php
@@ -1,1 +1,15 @@
 <!--Page de la premiere question-->
+<?php
+//récupération des données simples
+echo $_POST['titre'].'';
+echo $_POST['name'].'';
+//récupération des cases à coché
+if(isset($_POST['restart']))// si la case Start Quiz est cochée, c'est que le formulaire a envoyé quelque chose
+{
+  echo $_POST['restart'];
+}
+else
+{
+  echo "Quit";
+}
+?>
