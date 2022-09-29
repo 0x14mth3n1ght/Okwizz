3eb911fbd370f50424bbf288ff84658bcbae28f7
nabil <none@none.com>
2022-09-29 12:03:29 +0200

    changement mineur dans home.php

--- src/view/home.php
+++ src/view/home.php
@@ -2,6 +2,6 @@
 
 <p>Texte du mec qui doit ecrire les règles</p>
 
-<form action="question1.php" method="post">
+<form action="question.php" method="post">
 	<button type="submit">JOUER</button>
 </form>
