9ffd2b58687d6358d11b7e2e999486a5cc1cf261
willy <none@none.com>
2022-09-29 14:05:47 +0200

    Broullion de code pour les reponses au question

--- src/view/question.php
+++ src/view/question.php
@@ -1,1 +1,7 @@
 <!--Page de la premiere question-->
+<div class="AnswerPanel">
+    <button class="AnswerButton" type="submit">Answer 1</button>
+    <button class="AnswerButton" type="submit">Answer 2</button>
+    <button class="AnswerButton" type="submit">Answer 3</button>
+    <button class="AnswerButton" type="submit">Answer 4</button>
+</div>
