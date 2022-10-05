4ef01d267d9879475b0e3fb2d97458e00c9fe119
night <none@none.com>
2022-10-05 22:37:16 +0200

    traduction français

--- src/view/question.php
+++ src/view/question.php
@@ -35,17 +35,11 @@ Hello, <?php echo htmlspecialchars($_POST["name"]); ?>.
 <?php
 $answer=$_POST['AnswerQuestion1'];
 if($answer == "7"){
-<<<<<<< HEAD
 echo "Bravo !";
-}else{
-echo "Mauvaise réponse !";
-=======
-echo "Well Done !";
 }else if($answer != ""){
-echo "Wrong !";
+echo "Mauvaise réponse!";
 }else{
   echo $answer;
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
 }
 ?>
 </li>
@@ -53,28 +47,21 @@ echo "Wrong !";
 <div class="AnswerPanel">
 		<form action="../view/question.php" method="post">
       <input class="AnswerButton" type="submit" name="AnswerQuestion2" value="245"/>
-      <input class="AnswerButton" type="submit" name="AnswerQuestion2" value="365"/>
+      <input class="AnswerButton" type="submit" name="AnswerQuestion2" value="367"/>
       <input class="AnswerButton" type="submit" name="AnswerQuestion2" value="456"/>
       <input class="AnswerButton" type="submit" name="AnswerQuestion2" value="0"/>
 		</form>
 </div>
 <?php
 $answer=$_POST['AnswerQuestion2'];
-<<<<<<< HEAD
-if($answer == "365"){
-echo "Bravo !";
-}else{
-echo "Mauvaise réponse !";
-=======
 if($answer == "367"){
-echo "Well Done !";
+echo "Bravo !";
 }else if($answer != ""){
-echo "Wrong !";
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
+echo "Mauvaise réponse !";
 }
 ?>
 </li>
-<li>Combien de semaines il y a-t-il dans une année?  
+<li>Combien de semaines il y a-t-il dans une année? 
 <div class="AnswerPanel">
 		<form action="../view/question.php" method="post">
       <input class="AnswerButton" type="submit" name="AnswerQuestion3" value="52"/>
@@ -86,79 +73,45 @@ echo "Wrong !";
 <?php
 $answer=$_POST['AnswerQuestion3'];
 if($answer == "52"){
-<<<<<<< HEAD
 echo "Bravo !";
-}else{
-echo "Mauvaise réponse !";
-=======
-echo "Well Done !";
 }else if($answer != ""){
-echo "Wrong !";
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
+echo "Mauvaise réponse !";
 }
 ?>
 </li>
-<li>Quelle est la couleur du ciel? 
+<li>De quelle couleur est le ciel? 
 <div class="AnswerPanel">
 		<form action="../view/question.php" method="post">
-<<<<<<< HEAD
       <input class="AnswerButton" type="submit" name="AnswerQuestion4" value="bleu"/>
-      <input class="AnswerButton" type="submit" name="Answerquestion4" value="rouge"/>
-      <input class="AnswerButton" type="submit" name="Answerquestion4" value="vert"/>
-      <input class="AnswerButton" type="submit" name="Answerquestion4"" value="jaune"/>
-=======
-      <input class="AnswerButton" type="submit" name="AnswerQuestion4" value="blue"/>
-      <input class="AnswerButton" type="submit" name="AnswerQuestion4" value="red"/>
-      <input class="AnswerButton" type="submit" name="AnswerQuestion4" value="green"/>
-      <input class="AnswerButton" type="submit" name="AnswerQuestion4"" value="yellow"/>
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
+      <input class="AnswerButton" type="submit" name="AnswerQuestion4" value="rouge"/>
+      <input class="AnswerButton" type="submit" name="AnswerQuestion4" value="vert"/>
+      <input class="AnswerButton" type="submit" name="AnswerQuestion4"" value="jaune"/>
 		</form>
 </div>
 <?php
 $answer=$_POST['AnswerQuestion4'];
-<<<<<<< HEAD
 if($answer == "bleu"){
 echo "Bravo !";
-}else{
-echo "Mauvaise réponse !";
-=======
-if($answer == "blue"){
-echo "Well Done !";
 }else if($answer != ""){
-echo "Wrong !";
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
+echo "Mauvaise réponse !";
 }
 ?>
 </li>
-<li>Qui est le prédisent des États-Unis? 
+<li>Qui est le président des États-Unis? 
 <div class="AnswerPanel">
 		<form action="../view/question.php" method="post">
-<<<<<<< HEAD
       <input class="AnswerButton" type="submit" name="AnswerQuestion5" value="Obama"/>
-      <input class="AnswerButton" type="submit" name="Answerquestion5" value="Biden"/>
-      <input class="AnswerButton" type="submit" name="Answerquestion5" value="Vous"/>
-      <input class="AnswerButton" type="submit" name="Answerquestion5"" value="Willy"/>
-=======
-      <input class="AnswerButton" type="submit" name="AnswerQuestion5" value="obama"/>
-      <input class="AnswerButton" type="submit" name="AnswerQuestion5" value="trump"/>
-      <input class="AnswerButton" type="submit" name="AnswerQuestion5" value="you"/>
-      <input class="AnswerButton" type="submit" name="AnswerQuestion5"" value="willy"/>
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
+      <input class="AnswerButton" type="submit" name="AnswerQuestion5" value="Biden"/>
+      <input class="AnswerButton" type="submit" name="AnswerQuestion5" value="Vous"/>
+      <input class="AnswerButton" type="submit" name="AnswerQuestion5"" value="Willy"/>
 		</form>
 </div>
 <?php
 $answer=$_POST['AnswerQuestion5'];
-<<<<<<< HEAD
 if($answer == "Biden"){
 echo "Bravo !";
-}else{
-echo "Mauvaise réponse !";
-=======
-if($answer == "obama"){
-echo "Well Done !";
 }else if($answer != ""){
-echo "Wrong !";
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
+echo "Mauvaise réponse !";
 }
 ?>
 </li>
@@ -174,83 +127,49 @@ echo "Wrong !";
 <?php
 $answer=$_POST['AnswerQuestion6'];
 if($answer == "3"){
-<<<<<<< HEAD
 echo "Bravo !";
-}else{
-echo "Mauvaise réponse !";
-=======
-echo "Well Done !";
 }else if($answer != ""){
-echo "Wrong !";
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
+echo "Mauvaise réponse !";
 }
 ?>
 </li>
-<li>Quel est le premier élément de la classification périodique des éléments chimiques? 
+<li>Quel est le premier élément du tableau de la classification périodique? 
 <div class="AnswerPanel">
 		<form action="../view/question.php" method="post">
-<<<<<<< HEAD
-      <input class="AnswerButton" type="submit" name="AnswerQuestion7" value="hydrogene"/>
-      <input class="AnswerButton" type="submit" name="Answerquestion7" value="fer"/>
-      <input class="AnswerButton" type="submit" name="Answerquestion7" value="titane"/>
-      <input class="AnswerButton" type="submit" name="Answerquestion7"" value="cuivre"/>
-=======
-      <input class="AnswerButton" type="submit" name="AnswerQuestion7" value="hydrogen"/>
-      <input class="AnswerButton" type="submit" name="AnswerQuestion7" value="iron"/>
       <input class="AnswerButton" type="submit" name="AnswerQuestion7" value="titane"/>
+      <input class="AnswerButton" type="submit" name="AnswerQuestion7" value="fer"/>
+      <input class="AnswerButton" type="submit" name="AnswerQuestion7" value="hydrogene"/>
       <input class="AnswerButton" type="submit" name="AnswerQuestion7"" value="cuivre"/>
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
 		</form>
 </div>
 <?php
 $answer=$_POST['AnswerQuestion7'];
-<<<<<<< HEAD
 if($answer == "hydrogene"){
-echo "Bravo !";
-}else{
-echo "Mauvaise réponse !";
-=======
-if($answer == "hydrogen"){
-echo "Well Done !";
+echo "Bravo!";
 }else if($answer != ""){
-echo "Wrong !";
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
+echo "Mauvaise réponse !";
 }
 ?>
 </li>
 <li>Quelle substance produisent les abeilles? 
 <div class="AnswerPanel">
 		<form action="../view/question.php" method="post">
-<<<<<<< HEAD
+      <input class="AnswerButton" type="submit" name="AnswerQuestion8" value="eau"/>
       <input class="AnswerButton" type="submit" name="AnswerQuestion8" value="miel"/>
-      <input class="AnswerButton" type="submit" name="Answerquestion8" value="eau"/>
-      <input class="AnswerButton" type="submit" name="Answerquestion8" value="pollen"/>
-      <input class="AnswerButton" type="submit" name="Answerquestion8"" value="feu"/>
-=======
-      <input class="AnswerButton" type="submit" name="AnswerQuestion8" value="honey"/>
-      <input class="AnswerButton" type="submit" name="AnswerQuestion8" value="water"/>
       <input class="AnswerButton" type="submit" name="AnswerQuestion8" value="pollen"/>
-      <input class="AnswerButton" type="submit" name="AnswerQuestion8"" value="fire"/>
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
+      <input class="AnswerButton" type="submit" name="AnswerQuestion8"" value="feu"/>
 		</form>
 </div>
 <?php
 $answer=$_POST['AnswerQuestion8'];
-<<<<<<< HEAD
 if($answer == "miel"){
 echo "Bravo !";
-}else{
-echo "Mauvaise réponse !";
-=======
-if($answer == "honey"){
-echo "Well Done !";
 }else if($answer != ""){
-echo "Wrong !";
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
+echo "Mauvaise réponse !";
 }
 ?>
 </li>
-<li>Combien de bandes il y a-t-il sur le drapeau américain?
+<li>Combien de bandes il y a-t-il dans le drapeau américain?
 <div class="AnswerPanel">
 		<form action="../view/question.php" method="post">
       <input class="AnswerButton" type="submit" name="AnswerQuestion9" value="13"/>
@@ -262,15 +181,9 @@ echo "Wrong !";
 <?php
 $answer=$_POST['AnswerQuestion9'];
 if($answer == "13"){
-<<<<<<< HEAD
 echo "Bravo !";
-}else{
-echo "Mauvaise réponse !";
-=======
-echo "Well Done !";
 }else if($answer != ""){
-echo "Wrong !";
->>>>>>> 9f8e09a28cab442755d65be82570cb68d18677e4
+echo "Mauvaise réponse !";
 }
 ?>
 </li>
