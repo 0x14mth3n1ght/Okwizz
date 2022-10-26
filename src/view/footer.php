1f3fa0ee3587c3676fd8c53b8a39c9db997014f7
wacim <none@none.com>
2022-10-26 22:12:19 +0200

    Ajout de la nav bar a home.php + footer presque fini (problème de background couleur

---
+++ src/view/footer.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+    <title>Footer</title>
+</head>
+
+<body>
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>Compagny</h4>
+                    <ul>
+                        <li><a href="#">A propos</a></li>
+                        <li><a href="#">Nos services</a></li>
+                        <li><a href="#">Politique de confidentialité</a></li>
+                        <li><a href="#">Mention légal</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-col">
+                    <h4>Aide</h4>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Une erreur ?</a></li>
+                        <li><a href="#">Option de paiment</a></li>
+
+                    </ul>
+                </div>
+
+                <div class="footer-col">
+                    <h4>Vos quizz</h4>
+                    <ul>
+                        <li><a href="#">Un quizz, késako ?</a></li>
+                        <li><a href="#">Envoyez-nous vos quizz</a></li>
+                        <li><a href="#">Concours de quizz</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-col">
+                    <h4>Suivez-nous</h4>
+                    <div class="social-links">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </footer>
+</body>
+
+</html>
--- src/view/home.php
+++ src/view/home.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-	<link rel="stylesheet" href="../view/home.css">
+	<link rel="stylesheet" href="../view/footer.css">
 	<link rel="stylesheet" href="../view/stylesheet.css">
 
 	<!-- <link rel="stylesheet" href='https://fonts.googleapis.com/css?family=Montserrat'> -->
@@ -10,27 +10,11 @@
 
 <body>
 
-	<nav class="navbar">
-		<ul class="navbar_list">
-			<li class="navbar_listlogo">
-				<!-- <a href="#" class="logo">Scrum</a> -->
-				<img viewBox="0 0 640 512" width="100" title="scrum" src="/src/images/scrum-logo.png"> </img>
-			</li>
-			<li class="navbar_listitem"><a href="/src/public/index.php">Home</a></li>
-			<li class="navbar_listitem"><a href="h">Jouer</a>
-				<ul class="navbar_listitemdrop">
-					<li><a href="#">Partie rapide</a></li>
-					<li><a href="#">Multijoueurs</a></li>
-					<li><a href="#">Catégories</a></li>
-				</ul>
-			</li>
-			<li class="navbar_listitem"><a href="h">Contact</a></li>
-			<li class="navbar_listitem"><a href="h">Classement</a></li>
-			<li class="navbar_listitem"><a href="h">Profil</a></li>
-		</ul>
-		<!-- <img src="../view/hamburger-button-icons-menu-bar-line.png" alt="menu hamburger" class="menu-hamburger"> -->
-	</nav>
-
+	<?php
+	$title = 'Home';
+	$page = 'home';
+	include_once('header.php');
+	?>
 
 	<div class="info_box">
 		<!-- <label for="name">Pseudo :</label> -->
@@ -60,6 +44,11 @@
 			</div>
 		</form>
 	</div>
+
+	<?php
+	include_once('footer.php');
+	?>
+
 </body>
 
 </html>
