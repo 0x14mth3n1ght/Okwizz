9f83fc09263d0beb2e27159eb806247e7c69acca
wacim <none@none.com>
2022-10-27 11:26:31 +0200

    Header et home page en anglais

--- src/view/home.php
+++ src/view/home.php
@@ -19,13 +19,13 @@
 	<div class="info_box">
 		<!-- <label for="name">Pseudo :</label> -->
 		<div class="info_title">
-			<h2>Règles du jeu</h2>
+			<h2>Rules of the game</h2>
 		</div>
 
 		<div class="info_list">
-			<div class="info">1. Répondez aux questions suivantes en cochant les bonnes réponses, puis cliquez sur "valider".</div>
-			<div class="info">2. Vous avez 30 secondes pour répondre à chaque question</div>
-			<div class="info">3. Plus vous répondez vite, plus vous gagnez de points !</div>
+			<div class="info">1. Answer the following questions by ticking the correct answers, then click on "validate".</div>
+			<div class="info">2. You have 30 seconds to answer each question</div>
+			<div class="info">3. The faster you answer, the more points you get!</div>
 			<!-- <div class="info">4. Règle n°4</div> -->
 		</div>
 
