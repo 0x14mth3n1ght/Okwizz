90938387d36f5f3c828eccd7b582a9707c7e351f
nicolas <none@none.com>
2022-11-17 09:23:02 +0100

    fixe classement & remove unused file

--- src/public/classement.php
+++ src/public/classement.php
@@ -3,17 +3,19 @@ require_once 'template.php';
 require_once '../models/userManager.php';
 session_start();
 $scoreboard = UserManager::getAllUserHightscore();
-foreach($scoreboard as $key => $value){
-	if($value['pseudo'] == $_SESSION["name"]){
-		$id = $key;
+if(isset($_SESSION["name"])){
+	foreach($scoreboard as $key => $value){
+		if($value['pseudo'] == $_SESSION["name"]){
+			$id = $key;
+		}
 	}
-}
-if(isset($id)){
-	$vous = $scoreboard[$id];
-	unset($scoreboard[$id]);
-	$pseudo = $vous['pseudo'];
-	$vous['pseudo'] = "Vous ($pseudo)"; 
-	array_unshift($scoreboard, $vous);
+	if(isset($id)){
+		$vous = $scoreboard[$id];
+		unset($scoreboard[$id]);
+		$pseudo = $vous['pseudo'];
+		$vous['pseudo'] = "Vous ($pseudo)";
+		array_unshift($scoreboard, $vous);
+	}
 }
 loadview('classement', array(
 		'classement'
