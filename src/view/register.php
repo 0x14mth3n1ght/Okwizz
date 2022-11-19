3e5ede05a400493e70199f95c44b31564083e52c
nabil <none@none.com>
2022-11-19 15:27:37 +0100

    Ajout du backend de register, des warning à cause de constante depréciées à gérer plus tard

--- src/view/register.php
+++ src/view/register.php
@@ -1,8 +1,49 @@
-<form>
+<?php
+require_once '../models/userManager.php';
+require_once '../models/helpers.php';
+require_once  '../models/sanitization.php';
+require_once  '../models/validation.php';
+require_once  '../models/filter.php';
+$errors = [];
+$inputs = [];
+
+if (is_post_request()) {
+
+	$fields = [
+        'username' => 'string | required | alphanumeric | between: 3, 25 | unique: users, username',
+        'password' => 'string | required | secure',
+        'password2' => 'string | required | same: password'
+    ];
+	$messages = [
+        	'password2' => [
+            	'required' => 'Please enter the password again',
+            	'same' => 'The password does not match'
+        	]];
+	[$inputs, $errors] = filter($_POST, $fields, $messages);
+	if ($errors) {
+        	redirect_with('register.php', [
+            	'inputs' => $inputs,
+            	'errors' => $errors
+        	]);
+	}
+
+    	if (UserManager::registerPlayer($inputs['username'], $inputs['password'])){
+	redirect_with_message(
+            'login.php',
+            'Your account has been created successfully. Please login here.'
+        );
+	}
+}
+
+else if (is_get_request()) {
+    [$inputs, $errors] = session_flash('inputs', 'errors');
+}
+?>
+<form action="register.php" method="post">
 	<h1>Register</h1>
 
 	<div class="inputs">
-		<input type="text" placeholder="Username" required />
+		<input type="text" placeholder="Username" id="username" required />
 		<input type="password" placeholder="Password" id="password" required />
 		<input type="password" placeholder="Confirm Password" id="confirm_password" required />
 	</div>
@@ -18,4 +59,3 @@
 
 	<script src="../script/passwordValidity.js"></script>
 </form>
-
