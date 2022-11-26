3f900efbfbf1df96128208df0a90e87a5fdf9b1a
nabil <none@none.com>
2022-11-26 21:29:09 +0100

    correction de l'aquisition de la note mais probleme d'affichage peristant

--- src/public/review.php
+++ src/public/review.php
@@ -3,9 +3,11 @@ require_once 'template.php';
 require_once '../models/userManager.php';
 session_start();
 $Reviews=UserManager::getAllUserReview();
-$name = $_POST["name"];
-$rating = $_POST["rating"];
-$comment = $_POST["comment"];
-UserManager::setReview($name, $rating, $comment);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$name = $_POST["name"];
+	$rating = $_POST["rating"];
+	$comment = $_POST["comment"];
+	UserManager::setReview($name, $rating, $comment);
+}
 loadview('review', array('review'),$Reviews);
 ?>
--- src/view/review.php
+++ src/view/review.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" type="text/css" href="../style/rating_style.css">
-
-
-<script type="text/javascript">
+<img src="../images/star1.png" id="image1" >
+<img src="../images/star2.png" id="image2" >
+  <script type="text/javascript">
   
    function change(id)
    {
@@ -21,8 +21,6 @@
    }
 
 </script>
-
-
 <?php$comment = "";?>
 <h2>Tell us your thought about O'Kwiz !!</h2>
 <form method="post" action="../public/review.php">
@@ -30,18 +28,18 @@
 	<label>Name</label><input type="text" name="name" class="form-control" required><br><br/>
 	<label>Comment:</label> <textarea name="comment" rows="5" cols="40"></textarea><br><br/>
 <div class="div">
-	  <input type="hidden" name="rating" id="php1_hidden" value="1">
+	  <input type="hidden"  id="php1_hidden" value=1>
 	  <img src="../images/star1.png" onmouseover="change(this.id);" id="php1" class="php">
-	  <input type="hidden"  name="rating" id="php2_hidden" value="2">
+	  <input type="hidden"  id="php2_hidden" value=2>
 	  <img src="../images/star1.png" onmouseover="change(this.id);" id="php2" class="php">
-	  <input type="hidden"  name="rating" id="php3_hidden" value="3">
+	  <input type="hidden"  id="php3_hidden" value=3>
 	  <img src="../images/star1.png" onmouseover="change(this.id);" id="php3" class="php">
-	  <input type="hidden"  name="rating" id="php4_hidden" value="4">
+	  <input type="hidden"  id="php4_hidden" value=4>
 	  <img src="../images/star1.png" onmouseover="change(this.id);" id="php4" class="php">
-	  <input type="hidden"  name="rating" id="php5_hidden" value="5">
+	  <input type="hidden"  id="php5_hidden" value=5>
 	  <img src="../images/star1.png" onmouseover="change(this.id);" id="php5" class="php">
   </div>	
-
+	<input type="hidden" name="rating" id="phprating" value="0">
 	<label>Add my review</label><div><input type="submit" name="add"> </div>
 </form>
 
