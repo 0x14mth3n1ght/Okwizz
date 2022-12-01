ad220074fb2092be86c9e7db9dbc4a29627225bd
nicolas <none@none.com>
2022-12-01 09:42:11 +0100

    fixe formating

--- src/view/resultat.php
+++ src/view/resultat.php
@@ -25,10 +25,10 @@ extract($data);
 		<div class="buttons">
 			<button type="submit">Ranking</button>
 		</div>
-        </form>
+	</form>
 	<form action="../public/review.php" method="post">
-        <div class="buttons">
-                <button type="submit">Review</button>
-        </div>
+		<div class="buttons">
+			<button type="submit">Review</button>
+		</div>
 	</form>
 </div>
