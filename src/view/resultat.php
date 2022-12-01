32cf4d3dfd79d8ee41c1e3a3300fbee0ff03828d
nicolas <none@none.com>
2022-12-01 02:16:23 +0100

    split resultat view into a view + a controler

--- src/view/resultat.php
+++ src/view/resultat.php
@@ -1,20 +1,11 @@
+<?php
+extract($data);
+?>
+
 <div class="info_box">
 	<p>
-		Hi , <?php echo htmlspecialchars($_SESSION["name"]); ?> !
+		Hi , <?php echo htmlentities($pseudo); ?> !
 	</p>
-
-	<?php
-	require_once '../models/userManager.php';
-	require_once '../models/quizzManager.php';
-	$score = $_SESSION["score"];
-	$hscore = UserManager::getHighscore($_SESSION["name"]);
-	if ($score > $hscore)
-		UserManager::setHighscore($_SESSION["name"], $score);
-	UserManager::incNbparties($_SESSION["name"]);
-	if (isset($_SESSION["quizz_id"])) {
-		QuizzManager::incNbparties($_SESSION["quizz_id"]);
-	}
-	?>
 
 	<div>
 		<p>
@@ -24,7 +15,6 @@
 			Your highest Score: <?php echo $hscore ?>
 		</p>
 	</div>
-
 
 	<form action="../public/index.php" method="post">
 		<div class="buttons">
