8ff09236d81b67251c86506c42a1bade85af18d1
nicolas <none@none.com>
2022-12-01 02:17:14 +0100

    fixe question & awnser views support Quizz object

--- src/view/question.php
+++ src/view/question.php
@@ -1,34 +1,34 @@
+<?php
+extract($data);
+?>
+
 <div class="info_box">
-	<p> Bonjour <?php echo htmlspecialchars($_SESSION["name"]); ?>: <?php echo $_SESSION["score"]; ?>pts. </p>
+	<p> Bonjour <?php echo htmlentities($pseudo); ?>: <?php echo $score; ?>pts. </p>
 	<div class="info_title">
-		<p><?php echo $data['info_question']["question"]; ?></p>
+		<p><?php echo htmlentities($question->getQuestion()); ?></p>
 	</div>
 	<h1 id="chrono"></h1>
 	<script src="../script/chrono.js"></script>
-	<form action="../public/questionController.php" method="post">
+	<form action="awnser.php" method="post">
 		<div class="info_list">
 			<?php
-			foreach($data['info_question']["answers"] as $a){
-				?>
+			$awnsers = $question->getAllAwnsers();
+			for ($i = 0; $i < count($awnsers); ++$i) {
+				$a = htmlentities($awnsers[$i]);
+			?>
 				<div class="info">
-					<input type="radio" id="<?php echo $a; ?>" name="choix" value="<?php echo $a; ?>" required>
-					<label for="<?php echo $a; ?>"><?php echo $a; ?></label>
+					<input type="radio" id="choix_<?php echo $i; ?>" name="choix" value="<?php echo $i; ?>" required>
+					<label for="choix_<?php echo $i; ?>"><?php echo $a; ?></label>
 				</div>
-				<?php
+			<?php
 			}
 			?>
 		</div>
-		<?php
-		if(!isset($data['choix'])){
-			?>
-			<div class="buttons">
-				<button type="submit">Envoyer la réponse</button>
-			</div>
-			<?php
-		}
-		?>
+		<div class="buttons">
+			<button type="submit">Envoyer la réponse</button>
+		</div>
 	</form>
-	<form action="../public/index.php" method="post">
+	<form action="index.php" method="post">
 		<div class="buttons">
 			<button type="submit">Main Menu</button>
 		</div>
